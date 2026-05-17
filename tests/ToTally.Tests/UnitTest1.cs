864fd834f34aaa983f5ee67cfea75722dd42c356
Kenneth Bonte <Kenneth.Bonte@indigobeam.com>
2026-05-17 16:27:44 -0500

    Playing with the unit testing framwork and making a basic 4 = 2+2 test and red green refactoring with it a bit

--- tests/ToTally.Tests/UnitTest1.cs
+++ tests/ToTally.Tests/UnitTest1.cs
@@ -3,8 +3,15 @@
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Test_ProjectCanRunBasicAssertion()
     {
+        // Arrange
+        var expected = 4;
 
+        // Act
+        var actual = 2 + 2;
+
+        // Assert
+        Assert.Equal(expected, actual);
     }
 }
