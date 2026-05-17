683aacbdc99ecb4853f5d272742e2c5cbfe5a171
Kenneth Bonte <Kenneth.Bonte@indigobeam.com>
2026-05-17 16:53:17 -0500

    dotnet ef migrations add InitialCreate --project .\ToTally.Infrastructure --startup-project .\ToTally.Web

---
+++ ToTally.Infrastructure/Migrations/20260517215133_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ToTally.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "leagues",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    abbreviation = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_leagues", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_leagues_abbreviation",
+                table: "leagues",
+                column: "abbreviation",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "leagues");
+        }
+    }
+}
